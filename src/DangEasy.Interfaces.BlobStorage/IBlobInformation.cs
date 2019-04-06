ff8dd3ac26c36995b98e7ffb630182322c20a23c
AnthonyCogworks <anthony.dang@thecogworks.com>
2019-04-06 12:51:12 +0100

    Opinionated interface

---
+++ src/DangEasy.Interfaces.BlobStorage/IBlobInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DangEasy.Interfaces.BlobStorage
+{
+    public interface IBlobInformation
+    {
+        string AbsoluteUri { get; set; }
+        string Path { get; set; }
+        DateTime Created { get; set; }
+        DateTime Modified { get; set; }
+        long Size { get; set; }
+    }
+}
---
+++ src/DangEasy.Interfaces.BlobStorage/IBlobStorageClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DangEasy.Interfaces.BlobStorage
+{
+    public interface IBlobStorageClient
+    {
+        Task<bool> CreateContainerAsync(string containerName);
+        Task<bool> DeleteAsync(string path);
+        Task<bool> ExistsAsync(string path);
+        Task<Stream> GetAsync(string path);
+        Task<IBlobInformation> GetInfoAsync(string path);
+        Task<IEnumerable<string>> GetListAsync(string path);
+        Task<bool> SaveAsync(string filePath, Stream stream);
+    }
+}
